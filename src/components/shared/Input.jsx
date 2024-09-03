7cd2a1ee04c2aac94434b1bcbf957156c16198f4
ZUITOPIA <korhwngml123@naver.com>
2024-09-03 19:46:46 +0900

    fix: 입력된 할 일이 없을 때 alert 처리

--- src/components/shared/Input.jsx
+++ src/components/shared/Input.jsx
@@ -35,9 +35,11 @@ export default function Input({ addTodo }) {
 
   const handleAddTodo = (event) => {
     event.preventDefault();
-    if (inputValue.trim()) {
+    if (inputValue.trim() !== "") {
       addTodo(inputValue);
       setInputValue("");
+    } else {
+      alert("입력된 할 일이 없어요, 다시 확인해주세요!");
     }
   };
 
@@ -48,7 +50,7 @@ export default function Input({ addTodo }) {
           type="text"
           value={inputValue}
           onChange={handleChange}
-          placeholder="할 일을 입력하세요"
+          placeholder="할 일을 입력해주세요."
         />
         <Style.Button type="submit">+</Style.Button>
       </Style.InputWrapper>
